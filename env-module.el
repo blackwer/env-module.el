b2bd5a895a24a2cecba116c227019e66667af1dd
Robert Blackwell <rblackwell@flatironinstitute.org>
2020-03-13 10:32:32 -0400

    Initial commit

---
+++ env-module.el
@@ -0,0 +1,37 @@
+;;; env-module.el --- Wrapper for 'Environment Modules' to synchronize emacs environment with modules  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Robert Blackwell
+
+;; Author: Robert Blackwell <rblackwell@flatironinstitute.org>
+;; Keywords: processes, convenience
+
+(require 'eshell)
+
+(defun env-module-eval-string (string)
+  "Evaluate a string of elisp code for side effects."
+  (with-temp-buffer
+    (insert string)
+    (beginning-of-buffer)
+    (setq dellines (flush-lines "^[^(]"))
+    (eval-buffer)
+    ))
+
+(defun env-module-print (string)
+  "Evaluate a string of elisp code for side effects."
+  (with-temp-buffer
+    (insert string)
+    (beginning-of-buffer)
+    (flush-lines "^(")
+    (buffer-string)
+    ))
+
+(defun env-module (&rest args)
+  "Call 'module' command with associated options."
+  (setq args (eshell-flatten-and-stringify args))
+
+  (let ((cmd (concat "/usr/bin/tclsh /cm/local/apps/environment-modules/4.2.1/libexec/modulecmd.tcl lisp " args)))
+    (message cmd)
+    (let ((cmd-output (shell-command-to-string cmd)))
+      (env-module-eval-string cmd-output)
+      (env-module-print cmd-output)
+      )))
